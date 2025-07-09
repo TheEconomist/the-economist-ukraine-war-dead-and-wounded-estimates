8ceeab1bd5299485d208f79424644a87d118c098
Sondre Ulvund Solstad <sondresolstad@economist.com>
2025-07-09 13:13:28 +0100

    added test in anticipation of auto-updates being turned on

--- scripts/meta-estimate.R
+++ scripts/meta-estimate.R
@@ -13,14 +13,14 @@ sq_casualties <- read_csv('output-data/tracker/meta-estimate-casualties.csv')
 sq_deaths <- read_csv('output-data/tracker/meta-estimate-deaths.csv')
 
 # 2. Load source data ------------------------------------------------------------
-all <- read_csv('source-data/deaths-and-casualties-data/Soldier deaths_casualties in Ukraine - estimates.csv') %>% 
+all <- read_csv('source-data/deaths-and-casualties-data/Soldier deaths_casualties in Ukraine - estimates.csv') %>%
   filter(is.na(`ignore for chart, data from warring parties`)) %>%
   mutate(date = as.Date(date, format = '%d/%m/%Y'),
          source = ifelse(source == 'UK Ministry of Defence', 'UK MoD', source)) %>%
   mutate(source = ifelse(source == 'Pentagon leak', 'US DoD', source))
 all$source_id <- paste0('all_df_', all$source, '_', seq_len(nrow(all)))
 
-casualties_via_mod <- read_csv('source-data/deaths-and-casualties-data/Soldier deaths_casualties in Ukraine - uk mod month-by-month estimates.csv') %>% 
+casualties_via_mod <- read_csv('source-data/deaths-and-casualties-data/Soldier deaths_casualties in Ukraine - uk mod month-by-month estimates.csv') %>%
   mutate(date = as.Date(as.character(`date (at beginning of next month)`), format = '%d/%m/%Y')) %>%
   filter(date <= as.Date('2025-05-01')) %>%
   mutate(estimate_low = NA,
@@ -44,12 +44,12 @@ deaths_via_meduza <- read_csv('source-data/deaths-and-casualties-data/meduza_202
   # This chunk ensures that format stays consistent
   rename(date = Date,
          "Estimate based on probate data" = `Оценка на основе данных РНД`,
-         "Flash estimate (by date of discovery)" = `Оперативная оценка, прогноз`) %>%                    
-  mutate(estimate = ifelse(!is.na(`Estimate based on probate data`), 
+         "Flash estimate (by date of discovery)" = `Оперативная оценка, прогноз`) %>%
+  mutate(estimate = ifelse(!is.na(`Estimate based on probate data`),
                            `Estimate based on probate data`, `Flash estimate (by date of discovery)`),
-         estimate_low = ifelse(!is.na(ED_low), 
+         estimate_low = ifelse(!is.na(ED_low),
                                ED_low, ED_onlyprediction_low),
-         estimate_high = ifelse(!is.na(ED_high), 
+         estimate_high = ifelse(!is.na(ED_high),
                                 ED_high, ED_onlyprediction_high),
          country = 'russia') %>%
   arrange(date) %>%
@@ -58,15 +58,15 @@ deaths_via_meduza <- read_csv('source-data/deaths-and-casualties-data/meduza_202
 deaths_via_meduza$source_id <- paste0('deaths_via_meduza_', seq_len(nrow(deaths_via_meduza)))
 deaths_via_meduza$source <- 'Meduza'
 
-# We add on estimated deaths since: 
-deaths_via_meduza_weekly <- read_csv('source-data/deaths-and-casualties-data/meduza_2025_weekly.csv') %>% 
+# We add on estimated deaths since:
+deaths_via_meduza_weekly <- read_csv('source-data/deaths-and-casualties-data/meduza_2025_weekly.csv') %>%
   rename(date = Date) %>%
   filter(date <= as.Date('2024-11-18')) %>% # Removes some estimates marked as affected by reporting lags
-  mutate(estimate = ifelse(!is.na(`Estimate based on probate data`), 
+  mutate(estimate = ifelse(!is.na(`Estimate based on probate data`),
                            `Estimate based on probate data`, `Flash estimate (by date of discovery)`),
-         estimate_low = ifelse(!is.na(ED_low), 
+         estimate_low = ifelse(!is.na(ED_low),
                                ED_low, ED_prediction_low),
-         estimate_high = ifelse(!is.na(ED_high), 
+         estimate_high = ifelse(!is.na(ED_high),
                                 ED_high, ED_prediction_high),
          country = 'russia') %>%
   arrange(date) %>%
@@ -74,12 +74,12 @@ deaths_via_meduza_weekly <- read_csv('source-data/deaths-and-casualties-data/med
   filter(date > max(deaths_via_meduza$date)) %>%
   mutate(estimate = cumsum(estimate) + deaths_via_meduza$estimate[deaths_via_meduza$date == max(deaths_via_meduza$date)],
          estimate_low = cumsum(estimate_low) + deaths_via_meduza$estimate_low[deaths_via_meduza$date == max(deaths_via_meduza$date)],
-         estimate_high = cumsum(estimate_high) + deaths_via_meduza$estimate_high[deaths_via_meduza$date == max(deaths_via_meduza$date)]) 
+         estimate_high = cumsum(estimate_high) + deaths_via_meduza$estimate_high[deaths_via_meduza$date == max(deaths_via_meduza$date)])
 
 deaths_via_meduza_weekly$source_id <- paste0('deaths_via_meduza_weekly', seq_len(nrow(deaths_via_meduza_weekly)))
 deaths_via_meduza_weekly$source <- 'Meduza'
 
-deaths_via_meduza <- rbind(deaths_via_meduza[, intersect(names(deaths_via_meduza), names(deaths_via_meduza_weekly))], 
+deaths_via_meduza <- rbind(deaths_via_meduza[, intersect(names(deaths_via_meduza), names(deaths_via_meduza_weekly))],
                            deaths_via_meduza_weekly[, intersect(names(deaths_via_meduza), names(deaths_via_meduza_weekly))])
 
 # Inspect that this works
@@ -108,28 +108,28 @@ casualties_cumulative <- casualties_cumulative %>%
   filter(!source %in% 'UK MoD')
 
 # Merge in UK MoD data
-casualties_cumulative <- rbind(casualties_cumulative %>% 
+casualties_cumulative <- rbind(casualties_cumulative %>%
                                  select(-type, -`ignore for chart, data from warring parties`),
-                               casualties_via_mod[, colnames(casualties_via_mod) %in% colnames(casualties_cumulative)]) 
+                               casualties_via_mod[, colnames(casualties_via_mod) %in% colnames(casualties_cumulative)])
 # Drop the "Mediazona; Meduza" data already in the main dataset
 deaths_cumulative <- deaths_cumulative %>%
   filter(!source %in% c('Mediazona; Meduza'))
 
 # Merge in Meduza data:
-deaths_cumulative <- rbind(deaths_cumulative[, intersect(colnames(deaths_cumulative), 
-                                                         colnames(deaths_via_meduza))], 
-                           deaths_via_meduza[, intersect(colnames(deaths_cumulative), 
+deaths_cumulative <- rbind(deaths_cumulative[, intersect(colnames(deaths_cumulative),
+                                                         colnames(deaths_via_meduza))],
+                           deaths_via_meduza[, intersect(colnames(deaths_cumulative),
                                                          colnames(deaths_via_meduza))])
 
 # Finally, transfer both datasets to long format: we take lower, upper, and central estimates as our prediction target. This is not perfect: for a small number of sources we only have a lower bound.
-casualties_cumulative <- casualties_cumulative %>% 
-  pivot_longer(cols=c(estimate, estimate_low, estimate_high), 
-               names_to = 'estimate_type', 
+casualties_cumulative <- casualties_cumulative %>%
+  pivot_longer(cols=c(estimate, estimate_low, estimate_high),
+               names_to = 'estimate_type',
                values_to = 'estimate')
 
-deaths_cumulative <- deaths_cumulative %>% 
-  pivot_longer(cols=c(estimate, estimate_low, estimate_high), 
-               names_to = 'estimate_type', 
+deaths_cumulative <- deaths_cumulative %>%
+  pivot_longer(cols=c(estimate, estimate_low, estimate_high),
+               names_to = 'estimate_type',
                values_to = 'estimate')
 
 # 4. Load covariates and merge these in ---------------------------------------
@@ -148,7 +148,7 @@ covars_cumulative <- covars %>%
     total_daily_log_war_fires_to_date_in_russia_held_area = cumsum(ifelse(is.na(war_fires_per_day_in_russia_held_area), 0, ifelse(war_fires_per_day_in_russia_held_area > 0, log(war_fires_per_day_in_russia_held_area), 0))),
     total_cloud_cover_in_east_of_country_to_date = cumsum(ifelse(is.na(cloud_cover_in_country), 0, cloud_cover_in_country)),
     total_change_in_area_assessed_as_russia_controlled = cumsum(abs(ifelse(is.na(change_in_area_assessed_as_russia_controlled), 0, change_in_area_assessed_as_russia_controlled)))) %>%
-  
+
   # Select applicable covariates
   select(
     date,
@@ -186,33 +186,33 @@ deaths_cumulative <- deaths_cumulative %>%
 generate_gam_prediction <- function(estimate_df = casualties_cumulative,
                                     pred_df = covars_cumulative,
                                     gtitle = "Casualties",
-                                    gam_formula = estimate ~ 
-                                      
+                                    gam_formula = estimate ~
+
                                       # baseline trend using days since invasion
-                                      s(days_since_invasion, k = 10) + 
+                                      s(days_since_invasion, k = 10) +
                                       days_since_invasion +
-                                      
+
                                       # war-fires and visibility
-                                      te(total_daily_log_war_fires_to_date,          
+                                      te(total_daily_log_war_fires_to_date,
                                          total_cloud_cover_in_east_of_country_to_date, k = c(4, 4)) +
-                                      te(total_daily_log_war_fires_to_date_in_ukraine_held_area,          
+                                      te(total_daily_log_war_fires_to_date_in_ukraine_held_area,
                                          total_cloud_cover_in_east_of_country_to_date, k = c(4, 4)) +
-                                      te(total_daily_log_war_fires_to_date_in_russia_held_area,          
+                                      te(total_daily_log_war_fires_to_date_in_russia_held_area,
                                          total_cloud_cover_in_east_of_country_to_date, k = c(4, 4)) +
-                                      
+
                                       # change in territory over time
                                       s(total_change_in_area_assessed_as_russia_controlled, k = 5)){
-  
+
   library(mgcv)
   gam_model <- gam(
     formula = gam_formula,
     data    = estimate_df,
     weights = weight,
-    family  = tw(link = "log"),  
+    family  = tw(link = "log"),
     method  = "REML",
     select  = TRUE
   )
-  
+
   # Generate predictions:
   casualty_preds <- predict(
     gam_model,
@@ -220,10 +220,10 @@ generate_gam_prediction <- function(estimate_df = casualties_cumulative,
     type = "link",
     se.fit = TRUE
   )
-  
+
   # convert link-scale fits to response-scale means
   predictions <- tibble(date = seq.Date(from = invasion_start, to = Sys.Date(), by = "day")) %>%
-    mutate(days_since_invasion = as.numeric(date - min(date))) %>% 
+    mutate(days_since_invasion = as.numeric(date - min(date))) %>%
     mutate(
       fit_link = casualty_preds$fit,
       se_link  = casualty_preds$se.fit,
@@ -231,22 +231,22 @@ generate_gam_prediction <- function(estimate_df = casualties_cumulative,
       ci_lower = exp(fit_link - 1.96 * se_link),
       ci_upper = exp(fit_link + 1.96 * se_link)
     )
-  
+
   # retrieve Tweedie parameters
   phi   <- summary(gam_model)$dispersion
   p     <- gam_model$family$getTheta(TRUE)
   mu    <- predictions$fit  # on response scale
   var_y <- phi * mu^p
-  
+
   # on link scale, approximate pi-se:
   se_pi_link <- sqrt(predictions$se_link^2 + (var_y / mu^2))
-  
+
   predictions <- predictions %>%
     mutate(
       pi_low  = exp(fit_link - 1.96*se_pi_link),
       pi_high = exp(fit_link + 1.96*se_pi_link)
     )
-  
+
   # Plot
   p <- ggplot(predictions, aes(x = date)) +
     geom_ribbon(aes(ymin = pi_low, ymax = pi_high), alpha = 0.2) +
@@ -261,27 +261,27 @@ generate_gam_prediction <- function(estimate_df = casualties_cumulative,
     theme_minimal()+
     geom_point(data=estimate_df, aes(x=date, y=estimate))
   print(p)
-  
+
   # Smooth to lowest level of the estimates (7-day), ensure monotonically increasing
   predictions <- predictions %>% arrange(date)
   if(sum(duplicated(predictions$date))> 0){
     stop('Duplicated dates!')
   }
-  
-  
-  
+
+
+
   # Set a rolling average for the specified columns with dynamic window size
   library(zoo)
   for(i in c('fit', 'ci_lower', 'ci_upper', 'pi_low', 'pi_high')) {
     predictions[, i] <- rollapply(predictions[, i], width = 7, FUN = function(x) mean(x, na.rm = T), fill = NA, align = 'right', partial = TRUE)
   }
-  
+
   # Ensure all monotonically increasing
   for(i in c('fit', 'ci_lower', 'ci_upper', 'pi_low', 'pi_high')){
     predictions[, i] <- cummax(predictions[, i])
   }
-  
-  
+
+
   p <- ggplot(predictions, aes(x = date)) +
     geom_ribbon(aes(ymin = pi_low, ymax = pi_high), alpha = 0.2) +
     geom_ribbon(aes(ymin = ci_lower, ymax = ci_upper), fill = "steelblue", alpha = 0.3) +
@@ -295,9 +295,9 @@ generate_gam_prediction <- function(estimate_df = casualties_cumulative,
     theme_minimal()+
     geom_point(data=estimate_df, aes(x=date, y=estimate))
   print(p)
-  
+
   predictions <- predictions %>% rename(estimate = fit) %>% select(-fit_link, -se_link)
-  
+
   return(predictions)}
 
 # Casualties:
@@ -312,14 +312,19 @@ gam_deaths <- generate_gam_prediction(estimate_df = deaths_cumulative, gtitle='D
 
 # Test for large changes:
 for(i in c("estimate", "ci_lower", "ci_upper", "pi_low", "pi_high")){
-  if(max(gam_casualties[, i], na.rm = T) > max(sq_casualties[, i], na.rm = T) + 10000){
+  if(abs(max(gam_casualties[, i], na.rm = T) - max(sq_casualties[, i], na.rm = T)) > 10000){
     stop(paste('Problem for casualties data: unexpectedly large change in', i, '-- please inspect'))
   }
-  if(max(gam_deaths[, i], na.rm = T) > max(sq_deaths[, i], na.rm = T) + 5000){
+  if(abs(max(gam_deaths[, i], na.rm = T) - max(sq_deaths[, i], na.rm = T)) > 5000){
     stop(paste('Problem for deaths data: unexpectedly large change in', i, '-- please inspect'))
   }
+  if(abs(min(gam_casualties[, i], na.rm = T) - min(sq_casualties[, i], na.rm = T)) > 10000){
+    stop(paste('Problem for casualties data: unexpectedly large change in start of ', i, '-- please inspect'))
+  }
+  if(abs(min(gam_deaths[, i], na.rm = T) 0 min(sq_deaths[, i], na.rm = T)) > 5000){
+    stop(paste('Problem for deaths data: unexpectedly large change in start of', i, '-- please inspect'))
+  }
 }
-
 
 # Export data
 write_csv(gam_casualties %>% arrange(desc(date)), 'output-data/tracker/meta-estimate-casualties.csv')
@@ -332,12 +337,12 @@ ggplot(gam_casualties, aes(x = date)) +
   geom_ribbon(aes(ymin = ci_lower, ymax = ci_upper), fill = "steelblue", alpha = 0.3) +
   geom_line(aes(y = estimate), color = "steelblue", size = 1) +
   geom_point(data=casualties_cumulative, aes(x=date, y=estimate), color='steelblue', alpha = 0.5)+
-  
+
   geom_ribbon(data = gam_deaths, aes(ymin = pi_low, ymax = pi_high), alpha = 0.2) +
   geom_ribbon(data = gam_deaths, aes(ymin = ci_lower, ymax = ci_upper), fill = "darkblue", alpha = 0.3) +
   geom_line(data = gam_deaths, aes(y = estimate), color = "darkblue", size = 1) +
-  geom_point(data=deaths_cumulative, aes(x=date, y=estimate), color='darkblue', alpha = 0.5) + 
-  
+  geom_point(data=deaths_cumulative, aes(x=date, y=estimate), color='darkblue', alpha = 0.5) +
+
   labs(
     title = paste0("Predicted Russian casualties and deaths: mean ±95% CI (darker) and 95% PI (lighter)"),
     subtitle = 'Dots = data. Dark blue = deaths, light blue = casualties. Narrow band is confidence interval, wide is prediction interval',
