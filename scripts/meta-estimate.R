bc80e9624dfa5030e296d19a10bceb4e80079349
Sondre Ulvund Solstad <sondresolstad@economist.com>
2026-02-09 13:10:57 +0000

    Update meta-estimate.R

--- scripts/meta-estimate.R
+++ scripts/meta-estimate.R
@@ -335,7 +335,7 @@ if(tests){
                     'deaths new', 'deaths old'))
 
   for(i in c("estimate", "ci_lower", "ci_upper", "pi_low", "pi_high")){
-    if(abs(max(gam_casualties[, i], na.rm = T) - max(sq_casualties[, i], na.rm = T)) > 10000){
+    if(abs(max(gam_casualties[, i], na.rm = T) - max(sq_casualties[, i], na.rm = T)) > 20000){
       print(diagnostic_data)
       stop(paste('Problem for casualties data: unexpectedly large change in', i, '-- please inspect'))
     }
@@ -343,7 +343,7 @@ if(tests){
       print(diagnostic_data)
       stop(paste('Problem for deaths data: unexpectedly large change in', i, '-- please inspect'))
     }
-    if(abs(min(gam_casualties[, i], na.rm = T) - min(sq_casualties[, i], na.rm = T)) > 10000){
+    if(abs(min(gam_casualties[, i], na.rm = T) - min(sq_casualties[, i], na.rm = T)) > 20000){
       print(diagnostic_data)
       stop(paste('Problem for casualties data: unexpectedly large change in start of ', i, '-- please inspect'))
     }
